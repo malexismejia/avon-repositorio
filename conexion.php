d3287869cb554ac328be6ab3989bf49c501c762c
Alex Mejia <2537362011@mail.utec.edu.sv>
2020-05-18 17:01:03 -0500

    conf2

--- conexion.php
+++ conexion.php
@@ -1,23 +1,20 @@
 <?php
-/*$hostname='localhost';
-$database='avon';
-$username='root';
-$password='';
-*/
-$hostname='avon-server.mysql.database.azure.com';
-$database='avon';
-$username='avonadmin@avon-server';
-$password='etps1@@1234';
 
-//$password='avonFtp1234';
-
-$conexion=new mysqli($hostname,$username,$password,$database);
-if($conexion->connect_error){
-    printf($connect_error);
-    echo "El sitio web está experimentado problemas",$connect_error;
-}else{
-    echo "Exito";
+$link = mysqli_connect("avon-server.mysql.database.azure.com", "avonadmin@avon-server", "etps1@@1234", "avon");
+ 
+/* Comprueba la conexión */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
+ 
+if (!mysqli_query($link, "SET a=1")) {
+    printf("Errormessage: %s\n", mysqli_error($link));
+}
+ 
+/* Cierra la conexión */
+mysqli_close($link);
+
 ?>
 
 /*
